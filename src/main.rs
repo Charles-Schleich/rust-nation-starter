bc63b786a75b4075289265cc4d86745148cc0826
Dan Nixon <dan@dan-nixon.com>
2024-03-27 16:54:24 +0000

    Tidy motion control

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ async fn motion_step(
     };
 
     let direction = if bearing_to_target >= ANGLE_THRESHOLD {
-        println!("right");
-        Angle::right()
-    } else if bearing_to_target <= -10.0 {
         println!("left");
         Angle::left()
+    } else if bearing_to_target <= -10.0 {
+        println!("right");
+        Angle::right()
     } else {
         println!("straight");
         Angle::straight()
@@ -34,25 +34,17 @@ async fn motion_step(
     println!("motor: {:?}", ok);
 }
 
-fn bearing_to_target(vehicle_bearing: f64, target_bearing: f64) -> f64 {
-    vehicle_bearing - target_bearing
-}
-
 #[hs_hackathon::main]
 async fn main() -> eyre::Result<()> {
     let mut wheels = WheelOrientation::new().await?;
     let mut motor = MotorSocket::open().await?;
 //    let mut drone = Camera::connect().await?;
 
-    // Does this stop the motor driver being fucky?
-    // (spoiler: no)
-    motor.move_for(Velocity::forward(), std::time::Duration::from_secs(1)).await;
-
-    motion_step(&mut wheels, &mut motor, bearing_to_target(15.0, 30.0), 50.0).await;
+    motion_step(&mut wheels, &mut motor, 30.0, 50.0).await;
 
     tokio::time::sleep(std::time::Duration::from_secs(10)).await;
 
-    motion_step(&mut wheels, &mut motor, bearing_to_target(15.0, 2.0), 40.0).await;
+    motion_step(&mut wheels, &mut motor, -12.0, 40.0).await;
 
     Ok(())
 }
